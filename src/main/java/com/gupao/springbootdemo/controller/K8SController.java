a4823ebb1ed1880c049f68785d19d961ddf5f27b
liang233233 <liang233233>
2022-03-17 12:35:51 +0800

    push

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -1,5 +1,6 @@
 package com.gupao.springbootdemo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,11 @@ import java.net.InetAddress;
 @RestController
 public class K8SController {
 
+    @Value("${k8s.type}")
+    public String type;
 
     @RequestMapping("/k8s")
-    public String k8s(){
-        return "hello K8s <br/>111222 ";
+    public String k8s() {
+        return "hello K8s <br/>111222 " + type;
     }
 }
